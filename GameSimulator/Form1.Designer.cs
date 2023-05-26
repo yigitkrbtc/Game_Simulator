9f4ca642ae07efadff22c118d5873be3220b593c
yigitkrbtc <112809652+yigitkrbtc@users.noreply.github.com>
2023-05-26 20:29:50 +0300

    Add files via upload

---
+++ GameSimulator/Form1.Designer.cs
@@ -0,0 +1,276 @@
+﻿namespace GameSimulator
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gamecreating = new System.Windows.Forms.GroupBox();
+            this.creategamebutton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.player2NameTextBox = new System.Windows.Forms.TextBox();
+            this.player1NameTextBox = new System.Windows.Forms.TextBox();
+            this.gameloading = new System.Windows.Forms.GroupBox();
+            this.loadgamebutton = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.loadplayer2 = new System.Windows.Forms.TextBox();
+            this.loadplayer1 = new System.Windows.Forms.TextBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.gamescorelabel = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.score2TextBox = new System.Windows.Forms.TextBox();
+            this.score1TextBox = new System.Windows.Forms.TextBox();
+            this.simulategamebutton = new System.Windows.Forms.Button();
+            this.gamecreating.SuspendLayout();
+            this.gameloading.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gamecreating
+            // 
+            this.gamecreating.Controls.Add(this.creategamebutton);
+            this.gamecreating.Controls.Add(this.label2);
+            this.gamecreating.Controls.Add(this.label1);
+            this.gamecreating.Controls.Add(this.player2NameTextBox);
+            this.gamecreating.Controls.Add(this.player1NameTextBox);
+            this.gamecreating.Location = new System.Drawing.Point(25, 12);
+            this.gamecreating.Name = "gamecreating";
+            this.gamecreating.Size = new System.Drawing.Size(289, 140);
+            this.gamecreating.TabIndex = 0;
+            this.gamecreating.TabStop = false;
+            this.gamecreating.Text = "Creating Game";
+            // 
+            // creategamebutton
+            // 
+            this.creategamebutton.Location = new System.Drawing.Point(176, 103);
+            this.creategamebutton.Name = "creategamebutton";
+            this.creategamebutton.Size = new System.Drawing.Size(94, 25);
+            this.creategamebutton.TabIndex = 4;
+            this.creategamebutton.Text = "Create Game";
+            this.creategamebutton.UseVisualStyleBackColor = true;
+            this.creategamebutton.Click += new System.EventHandler(this.creategamebutton_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(28, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Player 2";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Player 1";
+            // 
+            // player2NameTextBox
+            // 
+            this.player2NameTextBox.Location = new System.Drawing.Point(96, 77);
+            this.player2NameTextBox.Name = "player2NameTextBox";
+            this.player2NameTextBox.Size = new System.Drawing.Size(174, 20);
+            this.player2NameTextBox.TabIndex = 1;
+            // 
+            // player1NameTextBox
+            // 
+            this.player1NameTextBox.Location = new System.Drawing.Point(96, 35);
+            this.player1NameTextBox.Name = "player1NameTextBox";
+            this.player1NameTextBox.Size = new System.Drawing.Size(174, 20);
+            this.player1NameTextBox.TabIndex = 0;
+            // 
+            // gameloading
+            // 
+            this.gameloading.Controls.Add(this.loadgamebutton);
+            this.gameloading.Controls.Add(this.label4);
+            this.gameloading.Controls.Add(this.label3);
+            this.gameloading.Controls.Add(this.loadplayer2);
+            this.gameloading.Controls.Add(this.loadplayer1);
+            this.gameloading.Location = new System.Drawing.Point(25, 168);
+            this.gameloading.Name = "gameloading";
+            this.gameloading.Size = new System.Drawing.Size(289, 140);
+            this.gameloading.TabIndex = 1;
+            this.gameloading.TabStop = false;
+            this.gameloading.Text = "Loading Game";
+            // 
+            // loadgamebutton
+            // 
+            this.loadgamebutton.Location = new System.Drawing.Point(176, 104);
+            this.loadgamebutton.Name = "loadgamebutton";
+            this.loadgamebutton.Size = new System.Drawing.Size(94, 25);
+            this.loadgamebutton.TabIndex = 5;
+            this.loadgamebutton.Text = "Load Game";
+            this.loadgamebutton.UseVisualStyleBackColor = true;
+            this.loadgamebutton.Click += new System.EventHandler(this.loadgamebutton_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(28, 85);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Player 2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(28, 43);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Player 1";
+            // 
+            // loadplayer2
+            // 
+            this.loadplayer2.Location = new System.Drawing.Point(96, 78);
+            this.loadplayer2.Name = "loadplayer2";
+            this.loadplayer2.Size = new System.Drawing.Size(174, 20);
+            this.loadplayer2.TabIndex = 3;
+            // 
+            // loadplayer1
+            // 
+            this.loadplayer1.Location = new System.Drawing.Point(96, 36);
+            this.loadplayer1.Name = "loadplayer1";
+            this.loadplayer1.Size = new System.Drawing.Size(174, 20);
+            this.loadplayer1.TabIndex = 2;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.gamescorelabel);
+            this.groupBox3.Controls.Add(this.label6);
+            this.groupBox3.Controls.Add(this.label5);
+            this.groupBox3.Controls.Add(this.score2TextBox);
+            this.groupBox3.Controls.Add(this.score1TextBox);
+            this.groupBox3.Controls.Add(this.simulategamebutton);
+            this.groupBox3.Location = new System.Drawing.Point(25, 314);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(289, 140);
+            this.groupBox3.TabIndex = 1;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Simulator";
+            // 
+            // gamescorelabel
+            // 
+            this.gamescorelabel.Font = new System.Drawing.Font("Big John", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gamescorelabel.Location = new System.Drawing.Point(30, 36);
+            this.gamescorelabel.Name = "gamescorelabel";
+            this.gamescorelabel.Size = new System.Drawing.Size(77, 78);
+            this.gamescorelabel.TabIndex = 5;
+            this.gamescorelabel.Text = "label7";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(132, 102);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(45, 13);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "Player 2";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(132, 68);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(45, 13);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "Player 1";
+            // 
+            // score2TextBox
+            // 
+            this.score2TextBox.Location = new System.Drawing.Point(176, 99);
+            this.score2TextBox.Name = "score2TextBox";
+            this.score2TextBox.Size = new System.Drawing.Size(83, 20);
+            this.score2TextBox.TabIndex = 2;
+            // 
+            // score1TextBox
+            // 
+            this.score1TextBox.Location = new System.Drawing.Point(176, 61);
+            this.score1TextBox.Name = "score1TextBox";
+            this.score1TextBox.Size = new System.Drawing.Size(83, 20);
+            this.score1TextBox.TabIndex = 1;
+            // 
+            // simulategamebutton
+            // 
+            this.simulategamebutton.Location = new System.Drawing.Point(176, 19);
+            this.simulategamebutton.Name = "simulategamebutton";
+            this.simulategamebutton.Size = new System.Drawing.Size(90, 24);
+            this.simulategamebutton.TabIndex = 0;
+            this.simulategamebutton.Text = "Start simulating";
+            this.simulategamebutton.UseVisualStyleBackColor = true;
+            this.simulategamebutton.Click += new System.EventHandler(this.simulategamebutton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(349, 500);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.gameloading);
+            this.Controls.Add(this.gamecreating);
+            this.Name = "Form1";
+            this.Text = "YK GAME SIMULATOR";
+            this.gamecreating.ResumeLayout(false);
+            this.gamecreating.PerformLayout();
+            this.gameloading.ResumeLayout(false);
+            this.gameloading.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gamecreating;
+        private System.Windows.Forms.GroupBox gameloading;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button creategamebutton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox player2NameTextBox;
+        private System.Windows.Forms.TextBox player1NameTextBox;
+        private System.Windows.Forms.Button loadgamebutton;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox loadplayer2;
+        private System.Windows.Forms.TextBox loadplayer1;
+        private System.Windows.Forms.Label gamescorelabel;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox score2TextBox;
+        private System.Windows.Forms.TextBox score1TextBox;
+        private System.Windows.Forms.Button simulategamebutton;
+    }
+}
+
---
+++ GameSimulator/Form1.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GameSimulator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            loadplayer1.ReadOnly = true;
+            loadplayer2.ReadOnly = true;
+            score1TextBox.ReadOnly = true;
+            score2TextBox.ReadOnly = true;
+            loadgamebutton.Enabled = false;
+            gamescorelabel.Text = " ";
+        }
+
+        private void creategamebutton_Click(object sender, EventArgs e)
+        {    
+            // Validate player names
+            string player1Name = player1NameTextBox.Text.Trim();
+            string player2Name = player2NameTextBox.Text.Trim();
+
+            if (string.IsNullOrEmpty(player1Name) || string.IsNullOrEmpty(player2Name))
+            {
+                MessageBox.Show("Player names cannot be empty.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (!IsValidName(player1Name) || !IsValidName(player2Name))
+            {
+                MessageBox.Show("Player names can only contain letters, numbers, and spaces.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (player1Name.Length > 20 || player2Name.Length > 20)
+            {
+                MessageBox.Show("Player names cannot exceed 20 characters.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            SaveFileDialog dialog = new SaveFileDialog();
+            if(dialog.ShowDialog()==DialogResult.OK){
+                SaveForm(dialog.FileName);
+            }
+
+        }
+
+        private void SaveForm(string fileName)
+        {
+            string player1Name = player1NameTextBox.Text.Trim();
+            string player2Name = player2NameTextBox.Text.Trim();
+            StringBuilder builder = new StringBuilder();
+            builder.Append(","+player1Name);
+            builder.Append(","+player2Name);
+            using (StreamWriter writer = new StreamWriter(fileName))
+            {
+                var content = builder.ToString();
+                writer.Write(content);
+                writer.Close();
+            }
+            MessageBox.Show("Player information saved successfully.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            loadgamebutton.Enabled = true;
+        }
+
+        private bool IsValidName(string name)
+        {
+            Regex regex = new Regex("^[a-zA-Z0-9 ]*$");
+            return regex.IsMatch(name);
+        }
+
+        private void loadgamebutton_Click(object sender, EventArgs e)
+        {
+
+            if (!File.Exists("data.txt"))
+            {
+                MessageBox.Show("Data file not found.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            try
+            {
+             
+                gamescorelabel.Text = "Waiting...";
+                loadplayer1.ReadOnly = true;
+                loadplayer2.ReadOnly = true;
+                gamescorelabel.Visible = true;
+                simulategamebutton.Enabled = true;
+            }
+            catch (IOException ex)
+            {
+                MessageBox.Show("Error reading data file: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            SaveFileDialog dialog = new SaveFileDialog();
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                OpenForm(dialog.FileName);
+            }
+        }
+
+        private void OpenForm(string fileName)
+        {
+            using (StreamReader sr = new StreamReader(fileName))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string line = sr.ReadLine();
+                    var LineArray = line.Split(',');
+                    loadplayer2.Text = LineArray[2];
+                    loadplayer1.Text = LineArray[1];
+                    
+
+                }
+            }
+        }
+
+        private void simulategamebutton_Click(object sender, EventArgs e)
+        {
+            if (player1NameTextBox.Text == "" || player2NameTextBox.Text == "")
+            {
+                MessageBox.Show("Please create or load a game before starting the simulator.");
+                gamescorelabel.Visible = false;
+                return;
+            }
+
+            creategamebutton.Enabled = false;
+            loadgamebutton.Enabled = false;
+
+            int countdown = 10;
+            gamescorelabel.Text = "Playing " + countdown.ToString();
+            simulategamebutton.Enabled = false;
+
+            Timer timer = new Timer();
+            timer.Interval = 1000;
+            timer.Tick += (object timerSender, EventArgs timerE) =>
+            {
+                countdown--;
+                if (countdown == 0)
+                {
+                    Random random = new Random();
+                    timer.Stop();
+                    int score1 = random.Next(16);
+                    int score2 = random.Next(16);
+                    score1TextBox.Text = score1.ToString();
+                    score2TextBox.Text = score2.ToString();
+                    if (score1 > score2)
+                    {
+                        gamescorelabel.Text = "Result " + player1NameTextBox.Text + " wins";
+                    }
+                    else if (score2 > score1)
+                    {
+                        gamescorelabel.Text = "Result " + player2NameTextBox.Text + " wins";
+                    }
+                    else
+                    {
+                        gamescorelabel.Text = "Result Tie";
+                    }
+                    creategamebutton.Enabled = true;
+                    loadgamebutton.Enabled = true;
+                    simulategamebutton.Enabled = true;
+                }
+                else
+                {
+                    gamescorelabel.Text = "Playing " + countdown.ToString();
+                }
+            };
+            timer.Start();
+        }
+    }
+}
